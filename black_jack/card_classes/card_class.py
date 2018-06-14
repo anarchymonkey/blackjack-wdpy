a415ff508d12315da99ea4754120a6155aa7deb5
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-06-14 22:59:41 +0530

    added function methods to Card class for random gen

--- black_jack/card_classes/card_class.py
+++ black_jack/card_classes/card_class.py
@@ -2,15 +2,31 @@
 docstring : Simple class for storing the whole card values , Total 53
 """
 import random
+from optparse import Values
 
 
 class Cards():
     """
     :All the values of cards
     """
+
     String_suits = 'Hearts Diamonds Spades Clubs'.split(' ')
     String_ranks = '2 3 4 5 6 7 8 9 10 Jack King Queen Ace'.split(' ')
-    Suits = list(String_suits)
-    Ranks = list(String_ranks)
-    Values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Jack': 10,
-              'King': 10, 'Queen': 10, 'Ace': 11}
+    String_values=' Two Three Four Five Six Seven Eight Nine King Queen Ace'.split(' ')
+
+    def __init__(self):
+        self.Suits = list(self.String_suits)
+        self.Ranks = list(self.String_ranks)
+        self.Value = list(self.String_values)
+        self.Values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9,
+                  'Jack': 10,
+                  'King': 10, 'Queen': 10, 'Ace': 11}
+
+    def __str__(self):
+        random.shuffle(self.Suits)
+        random.shuffle(self.Value)
+        return ' {} of {} '.format(self.Values[self.Value[0]] , self.Suits[0])
+
+
+card = Cards()
+print(card)
--- black_jack/card_decks/Deck.py
+++ black_jack/card_decks/Deck.py
@@ -3,12 +3,9 @@ docstring : This class is for the deck
 
 """
 
-from card_classes import card_class
-
 
 class Deck():
-    pass
 
-    deck = []
-    for card in card_class.Cards.Suits:
-        for cards in card_class.Cards.Ranks:
+'''
+Docstring: This contains the shuffled decks that are to be kept
+'''
