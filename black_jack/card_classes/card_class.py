ab77f0c6d2f9bf4c6f491c86c93a9f6e0d35c19a
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-06-15 14:56:55 +0530

    changes in classes for dictionary search value by card fold

--- black_jack/card_classes/card_class.py
+++ black_jack/card_classes/card_class.py
@@ -11,18 +11,16 @@ class Cards():
     """
 
     String_suits = 'Hearts Diamonds Spades Clubs'.split(' ')
-    String_ranks = '2 3 4 5 6 7 8 9 10 Jack King Queen Ace'.split(' ')
-    String_values=' Two Three Four Five Six Seven Eight Nine King Queen Ace'.split(' ')
+    String_ranks = 'Two Three Four Five Six Seven Eight Nine King Queen Ace'.split(' ')
 
     def __init__(self):
         self.Suits = list(self.String_suits)
         self.Ranks = list(self.String_ranks)
-        self.Value = list(self.String_values)
         self.Values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Jack': 10, 'King': 10, 'Queen': 10, 'Ace': 11}
 
     def __str__(self):
         random.shuffle(self.Suits)
-        random.shuffle(self.Value)
-        return ' {} of {} '.format(self.Values[self.Value[0]] , self.Suits[0])
+        random.shuffle(self.Ranks)
+        return " {} of {} ".format(self.Ranks[0], self.Suits[0] )
 
 car=Cards()
--- black_jack/card_decks/Deck.py
+++ black_jack/card_decks/Deck.py
@@ -10,23 +10,80 @@ class Deck(card_classes.card_class.Cards):
     deck = []
 
     def __init__(self):
+       """
+       Shuffles:keyword
+       deck[52:return
+       deck:var
+
+       """
+       card_classes.card_class.Cards.__init__(self)   #   initialized the class Cards from Package card_Classes
+
+       shuffle(card_classes.card_class.car.Suits)
+       """
+       Docstring: Shuffling all the suits
+       random:imports
+       """
+
+
+       for suit in card_classes.card_class.car.Suits:
 
 
 
+            """
+            Docstring: For loop to generate through the suits and ranks
+            
+            """
+            shuffle(card_classes.card_class.car.Ranks)
 
-        card_classes.card_class.Cards.__init__(self)
-
-        shuffle(card_classes.card_class.car.Suits)
-        shuffle(card_classes.card_class.car.Ranks)
-        for suit in card_classes.card_class.car.Suits:
+            """
+            
+            
+            Docstring: Shuffle all the ranks that are in order
+            
+            
+            """
             for rank in card_classes.card_class.car.Ranks:
+
+                """
+                
+                
+                Docstring: appending the suits and ranks of class Cards to this init 
+                
+                """
                 self.deck.append(rank +' of ' + suit)
 
+            """
+            
+            Docstring: Method to shuffle all decks when all 52 of them are there
+            
+            """
+    def shuffle_cards(self):
 
+        shuffle(self.deck)    #  Imported from random
+
+    """
+    
+    Docstring : These are the first cards dealt by the dealer
+    
+    """
+    def deal_cards(self):
+
+            return self.deck[:2:]
+
+"""
+
+docstring: Minor Error Testing by calling the values
+"""
 
 d=Deck()
-print(d.deck)
+d.shuffle_cards()
 
+dealt =d.deal_cards()
+print(dealt)
+
+deal1 =str(dealt[0]).split(' ')[0]
+deal2 =str(dealt[1]).split(' ')[0]
+print(card_classes.card_class.car.Values[deal1]+card_classes.card_class.car.Values[deal2])
 
 
 
