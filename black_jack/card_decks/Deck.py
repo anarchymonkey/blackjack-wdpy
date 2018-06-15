0167ff8de557ff7e55bd9c00f0a8d885868b28c1
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-06-15 15:56:08 +0530

    changed styling rules through official python doc.

--- black_jack/card_decks/Deck.py
+++ black_jack/card_decks/Deck.py
@@ -10,24 +10,21 @@ class Deck(card_classes.card_class.Cards):
     deck = []
 
     def __init__(self):
-       """
+        """
        Shuffles:keyword
        deck[52:return
        deck:var
 
        """
-       card_classes.card_class.Cards.__init__(self)   #   initialized the class Cards from Package card_Classes
+        card_classes.card_class.Cards.__init__(self)  # initialized the class Cards from Package card_Classes
 
-       shuffle(card_classes.card_class.car.Suits)
-       """
+        shuffle(card_classes.card_class.car.Suits)
+        """
        Docstring: Shuffling all the suits
        random:imports
        """
 
-
-       for suit in card_classes.card_class.car.Suits:
-
-
+        for suit in card_classes.card_class.car.Suits:
 
             """
             Docstring: For loop to generate through the suits and ranks
@@ -43,47 +40,55 @@ class Deck(card_classes.card_class.Cards):
             
             """
             for rank in card_classes.card_class.car.Ranks:
-
                 """
                 
                 
                 Docstring: appending the suits and ranks of class Cards to this init 
                 
                 """
-                self.deck.append(rank +' of ' + suit)
+                self.deck.append(rank + " of " + suit)
 
             """
             
             Docstring: Method to shuffle all decks when all 52 of them are there
             
             """
-    def shuffle_cards(self):
 
-        shuffle(self.deck)    #  Imported from random
+    def shuffle_cards(self):
+        """
+        Shuffled:var
+        None:return:
+
+        """
+
+        shuffle(self.deck)  # Imported from random
 
     """
     
     Docstring : These are the first cards dealt by the dealer
     
     """
-    def deal_cards(self):
 
-            return self.deck[:2:]
+    def deal_cards(self):
+        """
+
+        deck upto 2 :return:
+        """
+
+        return self.deck[:2:]
+
 
 """
 
 docstring: Minor Error Testing by calling the values
 """
 
-d=Deck()
-d.shuffle_cards()
+D = Deck()
+D.shuffle_cards()
 
-dealt =d.deal_cards()
-print(dealt)
+DEALT_CARDS = D.deal_cards()
+print(DEALT_CARDS)
 
-deal1 =str(dealt[0]).split(' ')[0]
-deal2 =str(dealt[1]).split(' ')[0]
-print(card_classes.card_class.car.Values[deal1]+card_classes.card_class.car.Values[deal2])
-
-
-
+DEAL_1 = str(DEALT_CARDS[0]).split(' ')[0]
+DEAL_2 = str(DEALT_CARDS[1]).split(' ')[0]
+print(card_classes.card_class.car.Values[DEAL_1] + card_classes.card_class.car.Values[DEAL_2])
