3fb66c0863f5868f3891556aaec9b3d4055cf9a9
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-06-15 19:33:14 +0530

    added Deal Handler class

---
+++ black_jack/Hand_class/Hands.py
@@ -0,0 +1,35 @@
+"""
+imports:keyword
+"""
+import card_decks
+import card_classes
+import card_classes.card_class
+import card_decks.Deck
+
+class Hands():
+    '''
+    card_add,ace_Adjust:returns
+    '''
+
+    def __init__(self):
+        """
+        cards,values,aces:keyword
+        aces ( 1 - 11) :returns
+        """
+    def card_add(self):
+        """
+
+        cards:return:
+        """
+
+
+
+    def ace_adjust(self):
+        """
+
+        aces:return:
+        """
+
+
+
+
