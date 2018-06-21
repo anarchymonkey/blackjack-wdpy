e440bb30b34a53b30be6033369094ff72ed435e6
anarchymonkey <aniketbatabyal1996@gmail.com>
2018-06-21 11:19:26 -0400

    added definition functions in the main class

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -2,3 +2,33 @@ import card_classes
 from card_classes import  card_class
 
 
+def take_bet():
+    pass
+
+
+
+
+def hit():
+    pass
+
+
+
+
+
+def hit_or_stand():
+    pass
+
+
+
+
+
+def show_sime():
+    pass
+
+
+
+def show_all():
+    pass
+
+
+
